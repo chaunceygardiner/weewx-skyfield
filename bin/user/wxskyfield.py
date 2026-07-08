037c750dacf767136b6b6ec3b9fa9aa2174fc0ab
John Kline <john@johnkline.com>
2026-07-08 13:44:50 -0700

    Release v1.5

--- bin/user/wxskyfield.py
+++ bin/user/wxskyfield.py
@@ -48,7 +48,7 @@ from weewx.units import ValueTuple
 # get a logger object
 log = logging.getLogger(__name__)
 
-WXSKYFIELD_VERSION = '1.4'
+WXSKYFIELD_VERSION = '1.5'
 
 if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 9):
     raise weewx.UnsupportedFeature(
